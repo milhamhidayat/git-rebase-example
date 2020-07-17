5ee78f518fde661297b2e7838f1b8ff1b29d87a0
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-17 14:06:14 +0700

    feat: init controller button

--- controller/controller.go
+++ controller/controller.go
@@ -2,3 +2,17 @@ package controller
 
 // Controller is an type alias for controller
 type Controller int
+
+const (
+	// Triangle is the first button
+	Triangle Controller = iota + 1
+
+	// Circle is the second button
+	Circle
+
+	// Cross is the third button
+	Cross
+
+	// Square is the fourth button
+	Square
+)
